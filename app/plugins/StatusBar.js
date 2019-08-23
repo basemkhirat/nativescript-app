ba4b08bcb2e4d9ae3ca1f1979b6750a6dea42a23
basem <basemkhirat@gmail.com>
2019-08-24 00:14:43 +0200

    fix separate plugins

--- app/plugins/StatusBar.js
+++ app/plugins/StatusBar.js
@@ -3,26 +3,21 @@ import * as platform from 'tns-core-modules/platform'
 import * as color from 'tns-core-modules/color'
 
 if (app.android) {
-
     app.on(app.displayedEvent, function (args) {
-        console.log("Event: " + args.eventName + ", Activity: " + args.activity + ", Bundle: " + args.bundle);
         if (app.android && platform.device.sdkVersion >= "21") {
             const window = app.android.foregroundActivity.getWindow();
             window.setStatusBarColor(new color.Color("#2c205c").android);
         }
     });
-
 }
 
 export default {
-
     setColor: (c) => {
         if (app.android && platform.device.sdkVersion >= "21") {
             const window = app.android.foregroundActivity.getWindow();
             window.setStatusBarColor(new color.Color(c).android);
         }
     }
-
 }
 
 
